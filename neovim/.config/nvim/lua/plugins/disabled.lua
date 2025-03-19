1990ebaaebff14944b80761c96267d6633b8e850
joppe <jboeve009@gmail.com>
2025-03-19 16:01:29 +0100

    disabled noice

--- neovim/.config/nvim/lua/plugins/disabled.lua
+++ neovim/.config/nvim/lua/plugins/disabled.lua
@@ -2,4 +2,6 @@ return {
 
   { "catppuccin/nvim", enabled = false },
   { "folke/tokyonight", enabled = false },
+  -- TODO enable noice but disable the vscode like command pallete.
+  { "folke/noice.nvim", enabled = false },
 }
