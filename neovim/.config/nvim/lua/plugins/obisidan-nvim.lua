db2dbe65f3010e8a5d19b875b241a1ddd3d16ed4
joppe <jboeve009@gmail.com>
2025-03-23 15:37:23 +0100

    Add obisidan-nvim

---
+++ neovim/.config/nvim/lua/plugins/obisidan-nvim.lua
@@ -0,0 +1,43 @@
+local vault_path = "~/Documents/obsidian_vault/vault-test/"
+
+return {
+  "obsidian-nvim/obsidian.nvim",
+  version = "*",  -- recommended, use latest release instead of latest commit
+  lazy = true,
+  ft = "markdown",
+  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+  -- event = {
+  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+  --   -- refer to `:h file-pattern` for more examples
+  --   "BufReadPre path/to/my-vault/*.md",
+  --   "BufNewFile path/to/my-vault/*.md",
+  -- },
+  dependencies = {
+    -- Required.
+    "nvim-lua/plenary.nvim",
+
+    -- see below for full list of optional dependencies 👇
+  },
+  opts = {
+    workspaces = {
+      {
+        name = "personal",
+        path = vault_path
+      },
+    },
+
+		picker = {
+			name = "snacks.pick"
+		},
+
+		completion = {
+			-- Enables completion using blink.cmp
+			blink = true,
+			-- Trigger completion at 2 chars.
+			min_chars = 2,
+		},
+
+    -- see below for full list of options 👇
+  },
+}
