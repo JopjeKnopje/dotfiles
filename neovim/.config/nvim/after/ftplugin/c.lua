0ec8b31c91313765a3c3be19ff57d015a7d7af4d
joppe <jboeve009@gmail.com>
2025-03-23 15:37:23 +0100

    Add obisidan-nvim

--- neovim/.config/nvim/after/ftplugin/c.lua
+++ neovim/.config/nvim/after/ftplugin/c.lua
@@ -6,3 +6,5 @@ opt.shiftwidth = 4
 local map = vim.keymap.set
 
 map("", "<leader>r", ":w | :!make run<CR>", { desc = "Run make run" })
+-- move to corresponding header
+map("n", "gh", ":ClangdSwitchSourceHeader<cr>", { desc = "Clangd: Switch between source and header" })
