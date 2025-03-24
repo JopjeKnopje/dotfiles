ca6adf0c803c6c010b9cede6f4492a5f7daabfbd
Joppe Boeve <jboeve009@gmail.com>
2025-03-24 13:33:47 +0100

    Disable linter for terraform

--- neovim/.config/nvim/lua/plugins/nvim-lint.lua
+++ neovim/.config/nvim/lua/plugins/nvim-lint.lua
@@ -1,10 +1,13 @@
--- Disable linter for markdown
+-- Disable linter for markdown and terraform
 return {
   {
     "mfussenegger/nvim-lint",
     opts = {
       linters_by_ft = {
+				-- TODO make this a toggable
         markdown = {},
+        terraform = {},
+        tf = {},
       },
     },
   },
