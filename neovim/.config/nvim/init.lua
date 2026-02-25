28e377b5b4bfaa3271eede662cd9fcdde3d11efa
Joppe Boeve <jboeve009@gmail.com>
2026-02-25 22:07:52 +0100

    Add Tiltfile as python

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -7,6 +7,13 @@ vim.filetype.add {
 	},
 }
 
+-- Add "support" for `Tiltfile`, this is purely done to get TreeSitter Syntax highlighting to work.
+vim.filetype.add {
+   filename = {
+     ['Tiltfile'] = 'py',
+   },
+}
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
