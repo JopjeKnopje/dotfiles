9d626c958f17f7dfdd0de7efd3fa19485d76690b
joppe <jboeve009@gmail.com>
2026-03-27 23:34:53 +0100

    Finalized

--- scripts/Scripts/scope-screen.sh
+++ scripts/Scripts/scope-screen.sh
@@ -1,10 +1,27 @@
+# How to use
+# 1. Set the IP address of your scope.
+# 2. Tweak the `FILE_NAME` if you'd like.
+# 3. Letsgoo!
+
 IP=192.168.1.200
 
 DATE=$(date +"%d-%m-%Y-%T")
 FILE_NAME="scope-${DATE}.png"
 
+
+DIR=$(mktemp -d)
+FILE_PATH="${DIR}/${FILE_NAME}"
+
 # Remove my xclip alias to type out the entire xclip command.
 unalias xclip 2>/dev/null
 
-echo ":DISPLAY:DATA? ON,OFF,PNG" | nc -w1 ${IP} 5555 | dd bs=1 skip=11 of=${FILE_NAME}
-xclip -selection clipboard -t image/png ${FILE_NAME}
+
+echo ":DISPLAY:DATA? ON,OFF,PNG" | nc -w1 ${IP} 5555 | dd bs=1 skip=11 of=${FILE_PATH}
+xclip -selection clipboard -t image/png ${FILE_PATH}
+echo "Save the screenshot in $PWD? Y/n"
+
+read ANSWER
+
+if [ "$ANSWER" = "y" ] || [ "$ANSWER" = "Y" ]; then
+	mv $FILE_PATH . -v
+fi
