04c2784c59453508e45c6df3f5d02d61a3cce707
Joppe Boeve <jboeve009@gmail.com>
2026-06-11 11:49:07 +0200

    monitor setup
    asd

---
+++ i3/.config/i3/work-layout.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --off --output HDMI-1 --off --output DP-1 --off --output DP-2 --off --output DP-1-1 --off --output DP-1-1-8 --mode 2560x1440 --pos 4480x0 --rotate normal --output DP-1-1-1 --primary --mode 2560x1440 --pos 1920x0 --rotate normal
