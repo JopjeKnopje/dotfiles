64e5b28f98d08be86cf478150fceaf101ced8ec9
joppe <jboeve009@gmail.com>
2026-06-12 23:02:04 +0200

    disable mouse accellration

---
+++ i3/.config/i3/disable-mouse-accel.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# source: https://wiki.tonytascioglu.com/scripts/i3/mouse_acceleration
+for id in $(xinput list | grep "pointer" | cut -d '=' -f 2 | cut -f 1); do xinput --set-prop $id 'libinput Accel Profile Enabled' 0, 1; done
